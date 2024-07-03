402b19548e9d18096590f738292f213bd3af5c9c
minhuisik <hesikmin94@naver.com>
2024-07-03 17:27:00 +0900

    first commit

---
+++ src/main/java/racketing/racket_service/RacketServiceApplication.java
@@ -0,0 +1,13 @@
+package racketing.racket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/racketing/racket_service/RacketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package racketing.racket_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
